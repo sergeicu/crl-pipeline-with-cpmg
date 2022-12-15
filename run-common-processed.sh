31d9e3a8630259a700920b7c1a3a8919e1b26864
Serge <serge.vasylechko@tch.harvard.edu>
2022-12-15 12:13:58 -0500

    bug fix - correct relevant path

--- run-common-processed.sh
+++ run-common-processed.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sh /fileserver/fastscratch/serge/code/mwf/experiments/s20211014_contralesions/data/serge/control1/crl/scripts/common/RunPipelineForOneSubject.sh
+sh scripts/common/RunPipelineForOneSubject.sh
